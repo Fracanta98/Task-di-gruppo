40c3acc4e7212e4f1e50f45491a851303d54f162
Roberto Capone <robertocapone2005@gmail.com>
2025-08-28 15:20:32 +0200

    mie modifiche

--- SubmainRoberto.java
+++ SubmainRoberto.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class SubmainRoberto {
     public static void main(String[] args){
 
         ArrayList <Prodotto> Prodotti = new ArrayList<>();
