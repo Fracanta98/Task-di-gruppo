1e8e00c0b9023b2cffb19c7d76be6a53b559071b
Roberto Capone <robertocapone2005@gmail.com>
2025-08-28 13:29:51 +0200

    aggiunta ProdottLibro

--- ProdottoLibro.java
+++ ProdottoLibro.java
@@ -1,3 +1,22 @@
-public class ProdottoLibro {
-    
+public class ProdottoLibro extends Prodotto {
+    private boolean studente;
+
+    public ProdottoLibro(String nome, double prezzoBase, boolean studente){
+    super (nome, prezzoBase);
+    this.studente = studente;
+}
+
+@Override
+public void calcolaPrezzoFinale(){
+    if(studente = true) {
+           double Prezzofinale = getPrezzoBase() - (getPrezzoBase()*0.15);
+           System.out.println("Prezzo: " + Prezzofinale);
+        }else{
+        System.out.println("Prezzo: " + getPrezzoBase());
+    }
+    }
+
+    public boolean switchStudente(){
+        return studente = !studente;
+    }
 }
