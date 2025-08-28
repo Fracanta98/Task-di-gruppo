dec05e6f7d79d0d98d6c0e3da27547e56f7d486e
Francesco Cantarelli <thefracanta98@hotmail.com>
2025-08-28 13:02:09 +0200

    creazione classe madre

--- Prodotto.java
+++ Prodotto.java
@@ -1,3 +1,25 @@
 public class Prodotto {
+    private String nome;
+    private double prezzoBase;
+
+
+
+    public Prodotto(String nome, double prezzoBase){
+        this.nome = nome;
+        this.prezzoBase = prezzoBase;
+    }
+
+
+    public double calcolaPrezzoFinale(){
+        return prezzoBase;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getPrezzoBase(){
+        return prezzoBase;
+    }
     
 }
